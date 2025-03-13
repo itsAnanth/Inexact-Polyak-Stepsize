3cd10b2d9c60c37c2ba4a30fa8f626d1f12dd3b7
itsAnanth <danonymousarmy@gmai.com>
2025-03-13 14:48:17 +0530

    validation set tracking

--- test/getData.py
+++ test/getData.py
@@ -11,8 +11,14 @@ def getData(dataset: str, batch_size=64):
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     trainset = data(root='./data', train=True, download=True, transform=transform)
     testset = data(root='./data', train=False, download=True, transform=transform)
+    
+    train_size = int(0.8 * len(trainset))
+    val_size = len(trainset) - train_size
+    
+    trainset, valset = torch.utils.data.random_split(trainset, [train_size, val_size])
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
     testloader = torch.utils.data.DataLoader(testset, batch_size=len(trainset), shuffle=True)
+    validationloader = torch.utils.data.DataLoader(valset, batch_size=len(valset), shuffle=False)
     
-    return trainloader, trainset, testloader, testset
+    return trainloader, trainset, testloader, testset, validationloader, valset
